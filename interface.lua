4cd3c794438245be0f71ed1b0ba88fa0a6247255
BrunoMine <borgesdossantosbruno@gmail.com>
2015-02-06 21:40:25 -0200

    Adicionado sistema do laptop
    Sistema de gerenciamento de apps

--- interface.lua
+++ interface.lua
@@ -1,7 +1,28 @@
 -- Interface principal do Laptop
 
+<<<<<<< HEAD
+-- Slots de aplicativos do laptop
+local brazutec_slot1_imagem = ""
+local brazutec_slot1_formspec = ""
+
+-- Instalador de aplicativos
+function brazutec_instalar_em_laptop(textura)
+	local brazutec_slot1_imagem = textura;
+	minetest.chat_send_all("Comando brazutec iniciado")
+	minetest.chat_send_all("Valor de brazutec_slot1_imagem eh " ..brazutec_slot1_imagem)
+	-- Agora ocorre a distribuição dos apps no laptop
+end
+
+-- Formspec do laptop
+brazutec_laptop = {
+	desktop = "size[12,9]".. -- Desktop
+		"bgcolor[#080808BB;true]"..
+		"image[0,0;15,10;brazutec_desktop.png]"..
+		"image[0,0;5,5;"..brazutec_slot1_imagem.."]",
+=======
 brazutec_laptop = { -- Formspec do laptop
 	desktop = "size[12,9]".. -- Desktop
 		"bgcolor[#080808BB;true]"..
 		"image[0,0;15,10;brazutec_desktop.png]",
+>>>>>>> origin/master
 }
