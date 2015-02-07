eb46f7f0f24ed1d07658e8526f3a27b7398431fe
BrunoMine <borgesdossantosbruno@gmail.com>
2015-02-07 19:27:44 -0200

    Reformulado sistema de atribuir slots
    Agora os slots usam etiquetas ao invés de formspecs, isso ajudará muito o precosso pois o botao levara direto para a função desejada pelo mod de app.

--- interface.lua
+++ interface.lua
@@ -1,104 +1,141 @@
 -- Interface principal do Laptop
 
 -- Slots de aplicativos do laptop
-local brazutec_slot1_imagem = "brazutec_botao_slotlivre.png"
-local brazutec_slot1_formspec = "brazutec_laptop.slotlivre"
-local brazutec_slot2_imagem = "brazutec_botao_slotlivre.png"
-local brazutec_slot2_formspec = "brazutec_laptop.slotlivre"
-local brazutec_slot3_imagem = "brazutec_botao_slotlivre.png"
-local brazutec_slot3_formspec = "brazutec_laptop.slotlivre"
-local brazutec_slot4_imagem = "brazutec_botao_slotlivre.png"
-local brazutec_slot4_formspec = "brazutec_laptop.slotlivre"
-local brazutec_slot5_imagem = "brazutec_botao_slotlivre.png"
-local brazutec_slot5_formspec = "brazutec_laptop.slotlivre"
-local brazutec_slot6_imagem = "brazutec_botao_slotlivre.png"
-local brazutec_slot6_formspec = "brazutec_laptop.slotlivre"
+local brazutec_slot1_imagem = ""
+local brazutec_slot1_etiqueta = ""
+local brazutec_slot2_imagem = ""
+local brazutec_slot2_etiqueta = ""
+local brazutec_slot3_imagem = ""
+local brazutec_slot3_etiqueta = ""
+local brazutec_slot4_imagem = ""
+local brazutec_slot4_etiqueta = ""
+local brazutec_slot5_imagem = ""
+local brazutec_slot5_etiqueta = ""
+local brazutec_slot6_imagem = ""
+local brazutec_slot6_etiqueta = ""
 brazutec_laptop = {
 	desktop = "size[12,9]"..
-		"bgcolor[#080808BB;true]"..
-		"image[0,0;15,10;brazutec_desktop.png]"..
-		"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";brazutec_slot1_botao;]"..
-		"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";brazutec_slot2_botao;]"..
-		"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";brazutec_slot3_botao;]"..
-		"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";brazutec_slot4_botao;]"..
-		"image_button[7,1.5;3,3;"..brazutec_slot5_imagem..";brazutec_slot5_botao;]"..
-		"image_button[8,4.5;3,3;"..brazutec_slot6_imagem..";brazutec_slot6_botao;]",
-	slotlivre = "size[5,6]".. -- Slot Livre
-		"label[0.5,0.5;Slot livre para aplicativos]"..
-		"image[0.5,1;5,5;brazutec_botao_slotlivre.png]",
+			"bgcolor[#080808BB;true]"..
+			"image[0,0;15,10;brazutec_desktop.png]"..
+			"image_button[10.3,6.8;2,2;brazutec_botao_semapps.png;brazutec_semapps_etiqueta;]",
+	semapps = "size[5,6.5]".. -- Tela de aviso 'Sem Apps'
+			"label[0.5,0.5;Nenhum aplicativo instalado]"..
+			"image[0.5,1;5,5;brazutec_botao_semapps.png]"..
+			"label[0.5,5.5;Encontre aplicativos no site]"..
+			"label[0.5,6;site]",
 }
 
 -- Instalador de aplicativos
-function brazutec_instalar_em_laptop(textura, formspec)
-	if brazutec_slot1_imagem == "brazutec_botao_slotlivre.png" then
+function brazutec_instalar_em_laptop(textura, etiqueta)
+	if brazutec_slot1_imagem == "" then
 		brazutec_slot1_imagem = textura
-		brazutec_slot1_formspec = formspec
+		brazutec_slot1_etiqueta = etiqueta
+		brazutec_laptop = {
+			desktop = "size[12,9]"..
+					"bgcolor[#080808BB;true]"..
+					"image[0,0;15,10;brazutec_desktop.png]"..
+					"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]",
+		}
 	 else
-		if brazutec_slot2_imagem == "brazutec_botao_slotlivre.png" then
+		if brazutec_slot2_imagem == "" then
 	 		brazutec_slot2_imagem = textura
-			brazutec_slot2_formspec = formspec
+			brazutec_slot2_etiqueta = etiqueta
+			brazutec_laptop = {
+				desktop = "size[12,9]"..
+					"bgcolor[#080808BB;true]"..
+					"image[0,0;15,10;brazutec_desktop.png]"..
+					"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]"..
+					"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";"..brazutec_slot2_etiqueta..";]",
+			}
 		 else
-		 	if brazutec_slot3_imagem == "brazutec_botao_slotlivre.png" then
+		 	if brazutec_slot3_imagem == "" then
 		 		brazutec_slot3_imagem = textura
-				brazutec_slot3_formspec = formspec
+				brazutec_slot3_etiqueta = etiqueta
+				brazutec_laptop = {
+					desktop = "size[12,9]"..
+						"bgcolor[#080808BB;true]"..
+						"image[0,0;15,10;brazutec_desktop.png]"..
+						"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]"..
+						"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";"..brazutec_slot2_etiqueta..";]"..
+						"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";"..brazutec_slot3_etiqueta..";]",
+				}
 			else
-			 	if brazutec_slot4_imagem == "brazutec_botao_slotlivre.png" then
+			 	if brazutec_slot4_imagem == "" then
 			 		brazutec_slot4_imagem = textura
-					brazutec_slot4_formspec = formspec
+					brazutec_slot4_etiqueta = etiqueta
+					brazutec_laptop = {
+						desktop = "size[12,9]"..
+							"bgcolor[#080808BB;true]"..
+							"image[0,0;15,10;brazutec_desktop.png]"..
+							"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]"..
+							"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";"..brazutec_slot2_etiqueta..";]"..
+							"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";"..brazutec_slot3_etiqueta..";]"..
+							"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";"..brazutec_slot4_etiqueta..";]",
+					}
 				else
-					if brazutec_slot5_imagem == "brazutec_botao_slotlivre.png" then
+					if brazutec_slot5_imagem == "" then
 			 			brazutec_slot5_imagem = textura
-						brazutec_slot5_formspec = formspec
+						brazutec_slot5_etiqueta = etiqueta
+						brazutec_laptop = {
+							desktop = "size[12,9]"..
+								"bgcolor[#080808BB;true]"..
+								"image[0,0;15,10;brazutec_desktop.png]"..
+								"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]"..
+								"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";"..brazutec_slot2_etiqueta..";]"..
+								"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";"..brazutec_slot3_etiqueta..";]"..
+								"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";"..brazutec_slot4_etiqueta..";]"..
+								"image_button[7,1.5;3,3;"..brazutec_slot5_imagem..";"..brazutec_slot5_etiqueta..";]",
+						}
 					 else
-					 	if brazutec_slot6_imagem == "brazutec_botao_slotlivre.png" then
+					 	if brazutec_slot6_imagem == "" then
 			 				brazutec_slot6_imagem = textura
-							brazutec_slot6_formspec = formspec
+							brazutec_slot6_etiqueta = etiqueta
+								brazutec_laptop = {
+									desktop = "size[12,9]"..
+										"bgcolor[#080808BB;true]"..
+										"image[0,0;15,10;brazutec_desktop.png]"..
+										"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]"..
+										"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";"..brazutec_slot2_etiqueta..";]"..
+										"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";"..brazutec_slot3_etiqueta..";]"..
+										"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";"..brazutec_slot4_etiqueta..";]"..
+										"image_button[7,1.5;3,3;"..brazutec_slot5_imagem..";"..brazutec_slot5_etiqueta..";]"..
+										"image_button[8,4.5;3,3;"..brazutec_slot6_imagem..";"..brazutec_slot6_etiqueta..";]"..
+										"image_button[10.3,6.8;2,2;brazutec_botao_excessodeapps.png;brazutec_excessodeapps_etiqueta;]",
+								}
+						 else
+						 	-- Excesso de aplicativos
+						 	brazutec_laptop = {
+									desktop = "size[12,9]"..
+										"bgcolor[#080808BB;true]"..
+										"image[0,0;15,10;brazutec_desktop.png]"..
+										"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";"..brazutec_slot1_etiqueta..";]"..
+										"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";"..brazutec_slot2_etiqueta..";]"..
+										"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";"..brazutec_slot3_etiqueta..";]"..
+										"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";"..brazutec_slot4_etiqueta..";]"..
+										"image_button[7,1.5;3,3;"..brazutec_slot5_imagem..";"..brazutec_slot5_etiqueta..";]"..
+										"image_button[8,4.5;3,3;"..brazutec_slot6_imagem..";"..brazutec_slot6_etiqueta..";]"..
+										"image_button[10.3,6.8;2,2;brazutec_botao_excessodeapps.png;brazutec_excessodeapps_etiqueta;]",
+									excessodeapps = "size[5,7]".. -- Tela de aviso de erro no app
+										"label[0.5,0.5;Excesso de aplicativos]"..
+										"image[0.5,1;5,5;brazutec_botao_semapps.png]"..
+										"label[0.5,5.5;O laptop suporta apenas 6]"..
+										"label[0.5,6;aplicativos]"..
+										"label[0.5,6.5;Existe um excesso (verifique)]",
+								}
 						end
 					end
 				end
 			end
 		end
 	end		
-	brazutec_laptop = {
-		desktop = "size[12,9]"..
-			"bgcolor[#080808BB;true]"..
-			"image[0,0;15,10;brazutec_desktop.png]"..
-			"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";brazutec_slot1_botao;]"..
-			"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";brazutec_slot2_botao;]"..
-			"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";brazutec_slot3_botao;]"..
-			"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";brazutec_slot4_botao;]"..
-			"image_button[7,1.5;3,3;"..brazutec_slot5_imagem..";brazutec_slot5_botao;]"..
-			"image_button[8,4.5;3,3;"..brazutec_slot6_imagem..";brazutec_slot6_botao;]",
-		slotlivre = "size[5,6]"..
-			"label[0.5,0.5;Slot livre para aplicativos]"..
-			"image[0.5,1;5,5;brazutec_botao_slotlivre.png]",
-	}
 end
 
--- Pegar formspec
-function pegar_formspec_laptop(slot_formspec)
-   return slot_formspec
-end
-
--- Funcoes dos botoes dos slots
+-- Funcoes dos botoes para ocorrencias
 minetest.register_on_player_receive_fields(function(player, meta, fields)
-	if fields.brazutec_slot1_botao then
-		minetest.show_formspec(player:get_player_name(), "", pegar_formspec_laptop(brazutec_slot1_formspec))
+	if fields.brazutec_semapps_etiqueta then
+		minetest.show_formspec(player:get_player_name(), "", brazutec_laptop.semapps)
 	end
-	if fields.brazutec_slot2_botao then
-		minetest.show_formspec(player:get_player_name(), "", pegar_formspec_laptop(brazutec_slot2_formspec))
+	if fields.brazutec_excessodeapps_etiqueta then
+		minetest.show_formspec(player:get_player_name(), "", brazutec_laptop.excessodeapps)
 	end
-	if fields.brazutec_slot3_botao then
-		minetest.show_formspec(player:get_player_name(), "", pegar_formspec_laptop(brazutec_slot3_formspec))
-	end
-	if fields.brazutec_slot4_botao then
-		minetest.show_formspec(player:get_player_name(), "", pegar_formspec_laptop(brazutec_slot4_formspec))
-	end
-	if fields.brazutec_slot5_botao then
-		minetest.show_formspec(player:get_player_name(), "", pegar_formspec_laptop(brazutec_slot5_formspec))
-	end
-	if fields.brazutec_slot6_botao then
-		minetest.show_formspec(player:get_player_name(), "", pegar_formspec_laptop(brazutec_slot6_formspec))
-	end
-	return false
 end)
