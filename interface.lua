07c42940cbd6ae0c65d96ea09780907b1dc24b36
Lunovox <rui.gravata@gmail.com>
2015-02-06 21:09:49 -0300

    Update interface.lua

--- interface.lua
+++ interface.lua
@@ -13,16 +13,7 @@ local brazutec_slot5_imagem = ""
 local brazutec_slot5_formspec = ""
 local brazutec_slot6_imagem = ""
 local brazutec_slot6_formspec = ""
-
--- Instalador de aplicativos
-function brazutec_instalar_em_laptop(textura, formspec)
-	brazutec_slot1_imagem = textura;
-	brazutec_slot1_formspec = formspec;
-	-- Agora ocorre a distribuição dos apps no laptop
-end
-
--- Formspec do laptop
-brazutec_laptop = {
+local brazutec_laptop = {
 	desktop = "size[12,9]".. -- Desktop
 		"bgcolor[#080808BB;true]"..
 		"image[0,0;15,10;brazutec_desktop.png]"..
@@ -33,3 +24,26 @@ brazutec_laptop = {
 		"image[0,0;5,5;"..brazutec_slot5_imagem.."]",
 		"image[0,0;5,5;"..brazutec_slot6_imagem.."]",
 }
+
+
+-- Instalador de aplicativos
+function brazutec_instalar_em_laptop(textura, formspec)
+	brazutec_slot1_imagem = textura;
+	brazutec_slot1_formspec = formspec;
+	-- Agora ocorre a distribuição dos apps no laptop
+	
+	-- Formspec do laptop
+	brazutec_laptop = {
+		desktop = "size[12,9]".. -- Desktop
+			"bgcolor[#080808BB;true]"..
+			"image[0,0;15,10;brazutec_desktop.png]"..
+			"image[0,0;5,5;"..brazutec_slot1_imagem.."]",
+			"image[0,0;5,5;"..brazutec_slot2_imagem.."]",
+			"image[0,0;5,5;"..brazutec_slot3_imagem.."]",
+			"image[0,0;5,5;"..brazutec_slot4_imagem.."]",
+			"image[0,0;5,5;"..brazutec_slot5_imagem.."]",
+			"image[0,0;5,5;"..brazutec_slot6_imagem.."]",
+	}
+end
+
+
