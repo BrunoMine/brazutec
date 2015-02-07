1ab0c2002bc8e06cef535b3f5af13d3649108030
BrunoMine <borgesdossantosbruno@gmail.com>
2015-02-07 09:19:13 -0200

    Reformulação da interface principal

--- interface.lua
+++ interface.lua
@@ -1,49 +1,90 @@
 -- Interface principal do Laptop
 
 -- Slots de aplicativos do laptop
-local brazutec_slot1_imagem = ""
-local brazutec_slot1_formspec = ""
-local brazutec_slot2_imagem = ""
-local brazutec_slot2_formspec = ""
-local brazutec_slot3_imagem = ""
-local brazutec_slot3_formspec = ""
-local brazutec_slot4_imagem = ""
-local brazutec_slot4_formspec = ""
-local brazutec_slot5_imagem = ""
-local brazutec_slot5_formspec = ""
-local brazutec_slot6_imagem = ""
-local brazutec_slot6_formspec = ""
-local brazutec_laptop = {
+local brazutec_slot1_imagem = "brazutec_botao_slotlivre.png"
+local brazutec_slot1_formspec = "brazutec_laptop.slotlivre"
+local brazutec_slot2_imagem = "brazutec_botao_slotlivre.png"
+local brazutec_slot2_formspec = "brazutec_laptop.slotlivre"
+local brazutec_slot3_imagem = "brazutec_botao_slotlivre.png"
+local brazutec_slot3_formspec = "brazutec_laptop.slotlivre"
+local brazutec_slot4_imagem = "brazutec_botao_slotlivre.png"
+local brazutec_slot4_formspec = "brazutec_laptop.slotlivre"
+local brazutec_slot5_imagem = "brazutec_botao_slotlivre.png"
+local brazutec_slot5_formspec = "brazutec_laptop.slotlivre"
+local brazutec_slot6_imagem = "brazutec_botao_slotlivre.png"
+local brazutec_slot6_formspec = "brazutec_laptop.slotlivre"
+brazutec_laptop = {
 	desktop = "size[12,9]".. -- Desktop
 		"bgcolor[#080808BB;true]"..
 		"image[0,0;15,10;brazutec_desktop.png]"..
 		"image[0,0;5,5;"..brazutec_slot1_imagem.."]",
-		"image[0,0;5,5;"..brazutec_slot2_imagem.."]",
-		"image[0,0;5,5;"..brazutec_slot3_imagem.."]",
-		"image[0,0;5,5;"..brazutec_slot4_imagem.."]",
-		"image[0,0;5,5;"..brazutec_slot5_imagem.."]",
-		"image[0,0;5,5;"..brazutec_slot6_imagem.."]",
+	slotlivre = "size[5,6]".. -- Slot Livre
+		"bgcolor[#080808BB;true]"..
+		"image[0.5,0.5;4,4;brazutec_botao_slotlivre.png]",
 }
-
 
 -- Instalador de aplicativos
 function brazutec_instalar_em_laptop(textura, formspec)
-	brazutec_slot1_imagem = textura;
-	brazutec_slot1_formspec = formspec;
-	-- Agora ocorre a distribuição dos apps no laptop
-	
-	-- Formspec do laptop
-	brazutec_laptop = {
-		desktop = "size[12,9]".. -- Desktop
+	if brazutec_slot1_imagem == "brazutec_botao_slotlivre.png" then
+		brazutec_slot1_imagem = textura
+		brazutec_slot1_formspec = formspec
+	 else
+	 	if brazutec_slot2_imagem == "brazutec_botao_slotlivre.png" then
+	 		brazutec_slot2_imagem = textura
+			brazutec_slot2_formspec = formspec
+		 else
+		 	if brazutec_slot3_imagem == "brazutec_botao_slotlivre.png" then
+		 		brazutec_slot3_imagem = textura
+				brazutec_slot3_formspec = formspec
+			 else
+			 	if brazutec_slot4_imagem == "brazutec_botao_slotlivre.png" then
+			 		brazutec_slot4_imagem = textura
+					brazutec_slot4_formspec = formspec
+				 else
+					if brazutec_slot5_imagem == "brazutec_botao_slotlivre.png" then
+			 			brazutec_slot5_imagem = textura
+						brazutec_slot5_formspec = formspec
+					 else
+					 	if brazutec_slot6_imagem == "brazutec_botao_slotlivre.png" then
+			 				brazutec_slot6_imagem = textura
+							brazutec_slot6_formspec = formspec
+						end
+					end
+				end
+			end
+		end
+	end		
+	brazutec_laptop = {-- Atualizando desktop
+		desktop = "size[12,9]"..
 			"bgcolor[#080808BB;true]"..
 			"image[0,0;15,10;brazutec_desktop.png]"..
-			"image[0,0;5,5;"..brazutec_slot1_imagem.."]",
-			"image[0,0;5,5;"..brazutec_slot2_imagem.."]",
-			"image[0,0;5,5;"..brazutec_slot3_imagem.."]",
-			"image[0,0;5,5;"..brazutec_slot4_imagem.."]",
-			"image[0,0;5,5;"..brazutec_slot5_imagem.."]",
-			"image[0,0;5,5;"..brazutec_slot6_imagem.."]",
+			"image_button[1,1.5;3,3;"..brazutec_slot1_imagem..";brazutec_slot1_botao;]"..
+			"image_button[2,4.5;3,3;"..brazutec_slot2_imagem..";brazutec_slot2_botao;]"..
+			"image_button[4,1.5;3,3;"..brazutec_slot3_imagem..";brazutec_slot3_botao;]"..
+			"image_button[5,4.5;3,3;"..brazutec_slot4_imagem..";brazutec_slot4_botao;]"..
+			"image_button[7,1.5;3,3;"..brazutec_slot5_imagem..";brazutec_slot5_botao;]"..
+			"image_button[8,4.5;3,3;"..brazutec_slot6_imagem..";brazutec_slot6_botao;]",
 	}
 end
 
-
+-- Funcoes dos botoes dos slots
+minetest.register_on_player_receive_fields(function(player,meta,fields)
+	if fields.brazutec_slot1_botao then
+		minetest.show_formspec(player:get_player_name(), "", brazutec_slot1_formspec)
+	end
+	if fields.brazutec_slot2_botao then --Vai para aventuras
+		minetest.show_formspec(player:get_player_name(), "", brazutec_slot2_formspec)
+	end
+	if fields.brazutec_slot3_botao then --Vai para casas
+		minetest.show_formspec(player:get_player_name(), "", brazutec_slot3_formspec)
+	end
+	if fields.brazutec_slot4_botao then --Vai para macros
+		minetest.show_formspec(player:get_player_name(), "", brazutec_slot4_formspec)
+	end
+	if fields.brazutec_slot5_botao then --Vai para bussolas
+		minetest.show_formspec(player:get_player_name(), "", brazutec_slot5_formspec)
+	end
+	if fields.brazutec_slot6_botao then --Vai para receitas
+		minetest.show_formspec(player:get_player_name(), "", brazutec_slot6_formspec)
+	end
+end)
