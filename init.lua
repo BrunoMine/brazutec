e04b25b1d1d0db9a51b97061853c7c00c9bdeb90
BrunoMine <borgesdossantosbruno@gmail.com>
2015-02-11 19:13:53 -0200

    Adicionado itens, receitas e diretrizes
    Esse novos arquivos servem para organizar melhor cada trecho de código

--- init.lua
+++ init.lua
@@ -1,8 +1,24 @@
+--[[
+
+Mod Brazutec
+Desenvolvido por BrunoMine da Comunidade Minetestbr <http://minetestbr.blogspot.com.br/>
+Git de colaboração e desenvolvimento <https://github.com/BrunoMine/brazutec/>
+
+]]--
+
+--
 -- Inicializando mod
+-- 
 
 print("[Brazutec] Carregando mod brazutec... ")
+dofile(minetest.get_modpath("brazutec").."/diretrizes.lua")
+print("[Brazutec] Diretrizes carregadas... ")
+dofile(minetest.get_modpath("brazutec").."/itens.lua")
+print("[Brazutec] Itens carregados... ")
 dofile(minetest.get_modpath("brazutec").."/node.lua")
 print("[Brazutec] Laptop carregado... ")
 dofile(minetest.get_modpath("brazutec").."/interface.lua")
 print("[Brazutec] Interface principal carregada... ")
+dofile(minetest.get_modpath("brazutec").."/receitas.lua")
+print("[Brazutec] Receitas carregadas... ")
 print("[Brazutec] OK")
