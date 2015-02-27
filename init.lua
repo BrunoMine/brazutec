271df6e0c2ecb7366c4ac34eb86050ea8be05b33
Lunovox <rui.gravata@gmail.com>
2015-02-26 21:15:35 -0300

    Nova forma de echo em terminal

--- init.lua
+++ init.lua
@@ -10,15 +10,10 @@ Git de colaboração e desenvolvimento <https://github.com/BrunoMine/brazutec/>
 -- Inicializando mod
 -- 
 
-print("[Brazutec] Carregando mod brazutec... ")
 dofile(minetest.get_modpath("brazutec").."/diretrizes.lua")
-print("[Brazutec] Diretrizes carregadas... ")
 dofile(minetest.get_modpath("brazutec").."/itens.lua")
-print("[Brazutec] Itens carregados... ")
 dofile(minetest.get_modpath("brazutec").."/node.lua")
-print("[Brazutec] Laptop carregado... ")
 dofile(minetest.get_modpath("brazutec").."/interface.lua")
-print("[Brazutec] Interface principal carregada... ")
 dofile(minetest.get_modpath("brazutec").."/receitas.lua")
-print("[Brazutec] Receitas carregadas... ")
-print("[Brazutec] OK")
+
+minetest.log('action',"[BRAZUTEC] carregado...")
